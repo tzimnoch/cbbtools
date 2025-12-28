f45e2a0b78126e18a391e39581e65ea0c6354289
Todd Zimnoch <tzimnoch@gmail.com>
2025-12-27 22:20:32 -0500

    Set application title. Use relative path for assets. Replace placeholder favicon with another placeholder favicon.

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -5,4 +5,10 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    assetPrefix: './'
+  },
+  html: {
+    title: 'CBB Tools'
+  }
 });
